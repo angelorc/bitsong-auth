a3e4455b1240b0784aabfa90be7d9e7ba72cb3d5
angelorc <angelo@rcstart.it>
2025-02-12 15:32:25 +0100

    feat: add logging to global authentication middleware for better debugging

--- packages/nuxt/src/runtime/middleware/auth.global.ts
+++ packages/nuxt/src/runtime/middleware/auth.global.ts
@@ -29,8 +29,10 @@ declare module 'vue-router' {
 }
 
 export default defineNuxtRouteMiddleware(async (to) => {
+  console.log('init middleware')
   // If auth is disabled, skip middleware
   if (to.meta?.auth === false) {
+    console.log('auth is disabled')
     return
   }
   const { loggedIn, options, fetchSession } = useAuth()
@@ -38,23 +40,33 @@ export default defineNuxtRouteMiddleware(async (to) => {
 
   // If guest mode, redirect if authenticated
   if (only === 'guest' && loggedIn.value) {
+    console.log('only guest')
     // Avoid infinite redirect
     if (to.path === redirectUserTo) {
+      console.log('[guest] avoid infinite redirect')
       return
     }
+    console.log('[guest] redirect to', redirectUserTo)
     return navigateTo(redirectUserTo)
   }
+  console.log('not only guest')
 
   // If client-side, fetch session between each navigation
   if (import.meta.client) {
+    console.log('client side')
     await fetchSession()
   }
   // If not authenticated, redirect to home
   if (!loggedIn.value) {
+    console.log('not logged in')
     // Avoid infinite redirect
     if (to.path === redirectGuestTo) {
+      console.log('avoid infinite redirect')
       return
     }
+    console.log('redirect to', redirectGuestTo)
     return navigateTo(redirectGuestTo)
   }
+
+  console.log('end middleware')
 })
