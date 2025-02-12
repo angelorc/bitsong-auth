2b89c79a2c0656e045ab257badcfa8738a77ae78
angelorc <angelo@rcstart.it>
2025-02-12 15:37:29 +0100

    feat: enhance logging in auth plugins for improved debugging and session management

--- packages/nuxt/src/runtime/plugins/auth.client.ts
+++ packages/nuxt/src/runtime/plugins/auth.client.ts
@@ -3,6 +3,11 @@ import { defineNuxtPlugin, useAuth } from '#imports'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   console.log('auth.client.ts')
+  console.log('[plugin] auth.client.ts - 1', nuxtApp.payload.serverRendered)
+  console.log('[plugin] auth.client.ts - 2.0', nuxtApp.payload.prerenderedAt)
+  console.log('[plugin] auth.client.ts - 2.1', Boolean(nuxtApp.payload.prerenderedAt))
+  console.log('[plugin] auth.client.ts - 3.0', nuxtApp.payload.isCached)
+  console.log('[plugin] auth.client.ts - 3.1', Boolean(nuxtApp.payload.isCached))
   if (!nuxtApp.payload.serverRendered) {
     console.log('auth.client.ts: !nuxtApp.payload.serverRendered')
     await useAuth().fetchSession()
