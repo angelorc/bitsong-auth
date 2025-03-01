88b6a2918d5e8805349f39814f57cf0e156ddb00
angelorc <angelo@rcstart.it>
2025-03-01 23:28:10 +0100

    feat: implement conditional database client initialization for development and production environments

--- apps/api/db/index.ts
+++ apps/api/db/index.ts
@@ -1,6 +1,6 @@
-// import { drizzle } from 'drizzle-orm/postgres-js'
-import { drizzle } from 'drizzle-orm/neon-http'
-// import postgres from 'postgres'
+import { drizzle as drizzle_pg } from 'drizzle-orm/postgres-js'
+import { drizzle as drizzle_neon } from 'drizzle-orm/neon-http'
+import postgres from 'postgres'
 import { neon } from '@neondatabase/serverless'
 import { config } from 'dotenv'
 import * as schema from './schema'
@@ -8,6 +8,17 @@ import * as schema from './schema'
 config({ path: '.env' })
 
 // const sql = postgres(`${process.env.DATABASE_URL}`, { ssl: 'require', max: 1 })
-// const sql = postgres(`${process.env.DATABASE_URL}`)
-const sql = neon(process.env.DATABASE_URL!)
-export const db = drizzle({ client: sql, schema })
+
+export const db = () => {
+  if (import.meta.dev) {
+    return drizzle_pg({
+      client: postgres(process.env.DATABASE_URL!),
+      schema,
+    })
+  }
+
+  return drizzle_neon({
+    client: neon(process.env.DATABASE_URL!),
+    schema,
+  })
+}
